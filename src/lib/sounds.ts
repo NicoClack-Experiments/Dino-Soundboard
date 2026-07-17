e065bd574ed385ee16653430566b933187e96c7f
Nico Clack <nico@bn7.net>
2026-07-17 22:04:45 +0100

    Fix masking and increase speed range

--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -12,8 +12,8 @@ export const soundCategories: SoundCategory[] = [
 	{
 		name: "Hiss",
 		color: "#e8aa00",
-		minSpeed: 0.85,
-		maxSpeed: 1.15,
+		minSpeed: 0.7,
+		maxSpeed: 1.3,
 		sounds: [
 			// link("sounds/hiss/1.mp3"),
 			// link("sounds/hiss/2.mp3"),
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { manifestGeneratorPlugin } from "sveltekit-adapter-versioned-worker";
 import { defineConfig } from "vite";
 
-const dev = process.env.NODE_ENV !== "production";
-const disableBaseURL = process.env.DISABLE_BASE_URL === "true";
-const base = dev || disableBaseURL ? "/" : "/Dino-Soundboard/";
-
 export default defineConfig({
-	base,
 	plugins: [manifestGeneratorPlugin(), sveltekit()],
 });
